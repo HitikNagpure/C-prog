e30a2e204f0d9d19be45340520ecb63f5c593c78
HritikNagpure <hritikrnagpure@gmail.com>
2025-05-21 19:29:10 +0530

    fibonachi

--- Q27.c
+++ Q27.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
 //Write a function to print n terms of the fibonacci sequence.
-// 0,1,1,2,3,5,,8,13,21,......
+// 0,1,1,2,3,5,8,13,21,......
+// using recursion
  
-int fibo(int n);
+int fib(int n);
 int main(){
+    int n;
+    scanf("%d", &n);
+
+    printf("fibonachi of %d is %d", n, fib(n));
 
     return 0;
 }
 
-int fibo()
+int fib(int n){
+    if(n == 0){
+        return 0;
+    }
+    if(n==1){
+        return 1; 
+    }
+
+    int fibNm1 = fib(n-1);
+    int fibNm2 = fib(n-2);
+    int fibN = fibNm1 + fibNm2;
+    return fibN;
+}
